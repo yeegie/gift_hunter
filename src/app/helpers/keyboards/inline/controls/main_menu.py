4f63e90fbd3979245a40dc79a66b2f1ef0e06eea
yeegie <lotus9200@gmail.com>
2025-07-25 15:33:45 +0600

    feat: added indicator for auto-buy status in main menu

--- src/app/helpers/keyboards/inline/controls/main_menu.py
+++ src/app/helpers/keyboards/inline/controls/main_menu.py
@@ -6,16 +6,19 @@ from app.helpers.fabric.controls import ControlsCallback
 from app.helpers.texts import menu
 
 
-def menu_keyboard():
+def menu_keyboard(auto_buy_enabled):
     builder = InlineKeyboardBuilder()
 
+    auto_buy_label = "🟢 Автопокупка" if auto_buy_enabled else "🔴 Автопокупка"
 
+    builder.row(InlineKeyboardButton(text=auto_buy_label, callback_data=ControlsCallback(action="", value="").pack()))
     builder.row(InlineKeyboardButton(text="👤 Профиль", callback_data=ControlsCallback(action="show_profile", value="").pack()))
     builder.row(
         InlineKeyboardButton(text="⭐️ Пополнить баланс", callback_data=ControlsCallback(action="show_deposit_dialog", value="").pack()),
         InlineKeyboardButton(text="⚙️ Настройки автопокупки", callback_data=ControlsCallback(action="auto_buy_settings", value="").pack())
     )
     builder.row(InlineKeyboardButton(text="🎁 Каталог", callback_data=ControlsCallback(action="show_catalog", value="").pack()))
+    builder.row(InlineKeyboardButton(text="🆘 Связь", callback_data=ControlsCallback(action="show_support", value="").pack()))
 
     return builder.as_markup(resize_keyboard=True)
 
