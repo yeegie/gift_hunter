292b8d66966f5e8ed9298ce1a3348336a7f68811
yeegie <lotus9200@gmail.com>
2025-07-25 15:33:16 +0600

    style: removed non-animation emojis

--- src/app/handlers/welcome.py
+++ src/app/handlers/welcome.py
@@ -12,5 +12,5 @@ from app.helpers.keyboards.inline.controls.main_menu import to_menu
 @user_router.message(CommandStart())
 async def welcome(message: Message, state: FSMContext):
     await state.clear()
-    await message.answer(random.choice('🤖 💎 👀 👻 👾 🎁 📦 ⭐️'.split(' ')))
+    await message.answer(random.choice('🤖 💎 👀 👻 ⭐️'.split(' ')))
     await message.answer(f"Привет, {message.from_user.first_name} 👋", reply_markup=to_menu())
