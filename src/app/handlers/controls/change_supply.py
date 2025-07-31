4b0f9c3695a999279aaf6ab7780cd8a052055fa5
yeegie <lotus9200@gmail.com>
2025-07-31 17:34:05 +0600

    feat(change_supply): reworked and restyled handler

--- src/app/handlers/controls/change_supply.py
+++ src/app/handlers/controls/change_supply.py
@@ -4,27 +4,40 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 
 from app.helpers.fabric.controls import ControlsCallback
-from app.helpers.keyboards.inline.controls.settings import to_settings
-
-from app.helpers.states.confirm_value import ConfirmAmountStates
+from app.helpers.keyboards.inline.controls.settings import controls_keyboard, to_settings
 
 from app.services.user.user import UserService
-from app.repositories.schemas.user import UserUpdateSchema, UserSettingsUpdateSchema
+from app.repositories.schemas.user import UserUpdateSchema, UserSchema
+from app.repositories.schemas.settings import SettingsUpdateSchema
 
 from app.handlers.routers import user_router
+from app.utils.functions.build_settings import build_settings_text
+
+from aiogram.fsm.state import StatesGroup, State
+
+
+class ConfirmSupplyStates(StatesGroup):
+    value = State()
 
 
 @user_router.callback_query(ControlsCallback.filter(F.action == "set_max_supply"))
-async def set_supply(callback: CallbackQuery, bot: Bot, state: FSMContext) -> None:
+async def set_supply(callback: CallbackQuery, bot: Bot, state: FSMContext, user_service: UserService) -> None:
+    user = await user_service.get_user(callback.from_user.id)
+
     await callback.message.edit_text(
-        text="Введите саплай",
+        text=(
+            "<b>Введите саплай</b>\n\n"
+            "💡 Саплай — это тираж выпущенных подарков, чем меньше тираж тем более ценным и дорогим считается подарок\n\n"
+            "Если не знаешь, что написать, вот тебе пример, обычно подарки выпускаются тиражами числом в 500К, 250К, самые дорогие бывают 1-20К\n\n"
+            f"💎 <b>Текущий саплай {user.settings.supply_limit}</b>"
+        ),
         reply_markup=to_settings()
     )
-    await state.set_state(ConfirmAmountStates.amount)
+    await state.set_state(ConfirmSupplyStates.value)
 
 
-@user_router.message(ConfirmAmountStates.amount)
-async def process_custom_amount(message: Message, state: FSMContext, user_service: UserService):
+@user_router.message(ConfirmSupplyStates.value)
+async def process_custom_supply(message: Message, state: FSMContext, user_service: UserService, user: UserSchema):
     try:
         amount = int(message.text)
         if amount <= 0:
@@ -33,9 +46,19 @@ async def process_custom_amount(message: Message, state: FSMContext, user_servic
     except ValueError:
         await message.answer("Это не число, введи ещё раз")
         return
-    
-    await user_service.change_settings(
-        user_id=message.from_user.id,
-        settings=UserSettingsUpdateSchema(max_supply=amount)
+
+    await user_service.change_user_settings(
+        message.from_user.id,
+        SettingsUpdateSchema(
+            supply_limit=amount
+        )
     )
     await state.clear()
+    await message.answer(build_settings_text(
+        auto_buy_current_status=user.settings.auto_buy,
+        min_price=user.settings.price_min,
+        max_price=user.settings.price_max,
+        supply_limit=user.settings.supply_limit,
+        cycles=user.settings.cycles,
+        quantity=user.settings.quantity,
+    ), reply_markup=controls_keyboard(user.settings.auto_buy))
