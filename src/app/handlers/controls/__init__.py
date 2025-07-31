ec8205eb55a84b47bb788450081f59f3fedc3d1a
yeegie <lotus9200@gmail.com>
2025-07-31 17:36:05 +0600

    chore: include handlers

--- src/app/handlers/controls/__init__.py
+++ src/app/handlers/controls/__init__.py
@@ -1,5 +1,10 @@
 from . import main_menu
 from . import auto_buy_settings
 from . import profile
-from . import change_price_range
+
+from . import toggle_auto_buy
+from . import change_quantity
+from . import change_cycles
+from . import change_min_price_range
+from . import change_max_price_range
 from . import change_supply
