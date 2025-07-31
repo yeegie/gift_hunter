72a7c5298ebad4a4061a4e21f4fa53a14ccd62f9
yeegie <lotus9200@gmail.com>
2025-07-31 17:33:15 +0600

    feat(change_cycles): reworked and restyled handler

---
+++ src/app/handlers/controls/change_cycles.py
@@ -0,0 +1,66 @@
+from aiogram import Bot, F
+from aiogram.filters import Command
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+
+from app.helpers.fabric.controls import ControlsCallback
+from app.helpers.keyboards.inline.controls.settings import controls_keyboard, to_settings
+
+from app.services.user.user import UserService
+from app.repositories.schemas.user import UserUpdateSchema, UserSchema
+from app.repositories.schemas.settings import SettingsUpdateSchema
+
+from app.handlers.routers import user_router
+from app.utils.functions.build_settings import build_settings_text
+
+from aiogram.fsm.state import StatesGroup, State
+
+from app.repositories.models.user import User
+
+
+class ConfirmCyclesStates(StatesGroup):
+    value = State()
+
+
+@user_router.callback_query(ControlsCallback.filter(F.action == "set_max_cycles"))
+async def set_cycle(callback: CallbackQuery, bot: Bot, state: FSMContext, user: User) -> None:
+    await callback.message.edit_text(
+        text=(
+            "✨ <b>Введите количество циклов</b>\n\n"
+            "🔄 Циклы — это то, сколько раз бот будет проходить по списку подарков, подходящих под ваши настройки.\n\n"
+            "💡 Например: есть 3 подходящих подарка, а вы указали 2 цикла — бот пройдётся по списку дважды и купит столько подарков, сколько указано в поле <b>«количество»</b>\n\n"
+            f"<b>Текущее значение {user.settings.cycles}</b>"
+        ),
+        reply_markup=to_settings()
+    )
+    await state.set_state(ConfirmCyclesStates.value)
+
+
+@user_router.message(ConfirmCyclesStates.value)
+async def process_custom_cycles(message: Message, state: FSMContext, user_service: UserService, user: UserSchema):
+    try:
+        amount = int(message.text)
+        if amount <= 0:
+            await message.answer("Значение должна быть положительным числом, напиши еще раз")
+            return
+    except ValueError:
+        await message.answer("Это не число, введи ещё раз")
+        return
+    
+    await user_service.change_user_settings(
+        message.from_user.id,
+        SettingsUpdateSchema(
+            cycles=amount
+        )
+    )
+
+    await state.clear()
+    await message.answer(build_settings_text(
+        auto_buy_current_status=user.settings.auto_buy,
+        min_price=user.settings.price_min,
+        max_price=user.settings.price_max,
+        supply_limit=user.settings.supply_limit,
+        cycles=user.settings.cycles,
+        quantity=user.settings.quantity,
+    ), reply_markup=controls_keyboard(user.settings.auto_buy))
+    
